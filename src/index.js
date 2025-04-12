b2c9bd71af0d33907441ba64a8d4966658991f7d
Pooja Arora <pooja@paperflite.com>
2025-04-13 02:33:04 +0530

    changed-listen
    changed-listen

--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
+// const express = require("express");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const routes = require("./routes");
+
+// dotenv.config();
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+
+// app.use((req, res, next) => {
+//   console.log(req.path, req.method)
+//   next()
+// })
+
+// app.use("/api", routes);
+
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const routes = require("./routes");
 
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
-
-// Add this authentication bypass middleware
-// app.use((req, res, next) => {
-//   // Check for API key in query parameter
-//   if (req.query.key === "2912") {
-//     return next();
-//   }
-//   // Continue with normal flow
-//   next();
-// });
-
-app.use("/api", routes);
-
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(req.path, req.method);
+  next();
 });
+
+app.use("/", routes);
+
+module.exports = app;
